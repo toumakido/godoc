579f1aa6a24a4b72052123ff002c5c113687ab5b
toumakido <ktcity14@gmail.com>
2025-01-22 17:49:13 +0900

    ls

--- os/main.go
+++ os/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
 	// 標準入力から相対パスを受け取って一覧表示(ディレクトリ想定で)
-	dir := "../"
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		log.Fatalf("failed to change directory: %s", dir)
-	}
-	for _, entry := range entries {
-		fmt.Printf("entry: %v\n", entry.Name())
+	var dir string
+	for {
+		n, err := fmt.Scanln(&dir)
+		if err != nil {
+			fmt.Printf("failed to scan standard input: %s", err.Error())
+		}
+		if n > 0 {
+			entries, err := os.ReadDir(dir)
+			if err != nil {
+				fmt.Printf("failed to read directory: %s", err.Error())
+			}
+			for _, entry := range entries {
+				fmt.Printf("%v\n", entry.Name())
+			}
+		}
 	}
 }
